0d79d36425a2647feb563bcabb153db00a583f70
吴宇 <adam_wu@qq.com>
2018-02-07 16:27:17 +0800

    g

---
+++ src/Ford/Escape/Escape2016.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 2018/2/7
+ * Time: 下午4:25
+ */
+namespace Ford\Escape;
+
+class Escape2016
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
