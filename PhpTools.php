ff4e0ba3e4611a6f272f48874b835afcc7a34b49
吴宇 <adam_wu@qq.com>
2018-02-07 15:37:01 +0800

    in order to build my packagist

--- PhpTools.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adam
- * Date: 2018/2/7
- * Time: 下午3:20
- */
-var_dump('123');
---
+++ src/Tools.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 2018/2/7
+ * Time: 下午3:30
+ */
+
+class Tools
+{
+    public function info(){
+        echo "This is Tools class";
+    }
+}
